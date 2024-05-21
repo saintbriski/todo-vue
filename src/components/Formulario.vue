fb5d44805f49062c2b567a1dc353436e85cfaf6c
Willian Briski <wyll_gustavo11@hotmail.com>
2024-05-21 14:28:54 -0300

    ajustando cabecalho.vue

---
+++ src/components/Formulario.vue
@@ -0,0 +1,20 @@
+<template>
+    <form @submit.prevent="cadastraTarefa">
+        <div class="row">
+            <div class="col">
+                <input :value="estado.tarefaTemp" @change="evento => estado.tarefaTemp = evento.target.value" required
+                    type="text" placeholder="Digite aqui a descrição da tarefa" class="form-control">
+            </div>
+            <div class="col-md-2">
+                <button type="submit" class="btn btn-primary">Cadastrar</button>
+            </div>
+            <div class="col-md-2">
+                <select @change="evento => estado.filtro = evento.target.value" class="form-control">
+                    <option value="todas">Todas as tarefas</option>
+                    <option value="finalizadas">Finalizadas</option>
+                    <option value="pendentes">Pendentes</option>
+                </select>
+            </div>
+        </div>
+    </form>
+</template>
